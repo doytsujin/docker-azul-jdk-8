eda0e9a275071deb4ac9057e0475c9a865d510d2
Ron Kurr <rkurr@overstock.com>
2018-09-29 14:23:54 +0000

    Upgrade to the current release of JDK 8.

--- tag-and-push.sh
+++ tag-and-push.sh
@@ -4,8 +4,8 @@
 UNIXTIME=$(date +%s)
 
 # docker tag SOURCE_IMAGE[:TAG] TARGET_IMAGE[:TAG]
-docker tag dockerazuljdk8_azul-jdk:latest kurron/docker-azul-jdk-8:latest
-docker tag dockerazuljdk8_azul-jdk:latest kurron/docker-azul-jdk-8:${UNIXTIME}
+docker tag docker-azul-jdk-8_azul-jdk:latest kurron/docker-azul-jdk-8:latest
+docker tag docker-azul-jdk-8_azul-jdk:latest kurron/docker-azul-jdk-8:${UNIXTIME}
 docker images
 
 # Usage:  docker push [OPTIONS] NAME[:TAG]
